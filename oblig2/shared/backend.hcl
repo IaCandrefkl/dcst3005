1253029f9752aeaf49e8cb8f105eb941606d36f9
Andrefkl <andre.klarpaas@gmail.com>
2025-10-27 18:28:19 +0100

    Fix outputs.tf

--- oblig2/shared/backend.hcl
+++ oblig2/shared/backend.hcl
@@ -2,4 +2,4 @@ resource_group_name  = "rg-tfstate-afk"
 storage_account_name = "sttfstateafk"
 container_name       = "tfstate"
 use_azuread_auth     = true
-use_msi              = true
+use_cli              = true
--- oblig2/terraform/outputs.tf
+++ oblig2/terraform/outputs.tf
@@ -1,16 +1,16 @@
 output "resource_group_name" {
   description = "Name of the resource group"
-  value       = azurerm_resource_group.main.name
+  value       = azurerm_resource_group.rg.name
 }
 
 output "storage_account_name" {
   description = "Name of the storage account"
-  value       = azurerm_storage_account.main.name
+  value       = azurerm_storage_account.sa.name
 }
 
 output "storage_account_endpoint" {
   description = "Primary blob endpoint"
-  value       = azurerm_storage_account.main.primary_blob_endpoint
+  value       = azurerm_storage_account.sa.primary_blob_endpoint
 }
 
 output "environment" {
